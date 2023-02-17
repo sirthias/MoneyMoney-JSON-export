5070a0c2163e6062e80f0e0771ef27e38d1a185a
Mathias <mathias@decodified.com>
2023-02-17 17:55:32 +0100

    Add export of all account fields

--- json-export.lua
+++ json-export.lua
@@ -7,7 +7,7 @@
 
 local encoding = "UTF-8"
 
-Exporter{version       = 1.01,
+Exporter{version       = 1.02,
          format        = "JSON file",
          fileExtension = "json",
          description   = "Export transactions as JSON file"}
@@ -21,39 +21,72 @@ local function jsonField (str)
   end
 end
 
+local function isoDate(date)
+  return MM.localizeDate("yyyy-MM-dd", date)
+end
+
+local function amount(value)
+  return MM.localizeAmount("0.00;-0.00", value)
+end  
+
 local comma
 
 function WriteHeader (account, startDate, endDate, transactionCount)
-  assert(io.write("[\n"))
+  assert(io.write(MM.toEncoding(encoding, '{\n' ..
+                                          '  "startDate": "' .. jsonField(isoDate(startDate)) .. '",\n' ..
+                                          '  "endDate": "' .. jsonField(isoDate(endDate)) .. '",\n' ..
+                                          '  "transactionCount": ' .. transactionCount .. ',\n' ..
+                                          '  "name": "' .. jsonField(account.name) .. '",\n' ..
+                                          '  "owner": "' .. jsonField(account.owner) .. '",\n' ..
+                                          '  "accountNumber": "' .. jsonField(account.accountNumber) .. '",\n' ..
+                                          '  "subAccount": "' .. jsonField(account.subAccount) .. '",\n' ..
+                                          '  "bankCode": "' .. jsonField(account.bankCode) .. '",\n' ..
+                                          '  "currency": "' .. jsonField(account.currency) .. '",\n' ..
+                                          '  "iban": "' .. jsonField(account.iban) .. '",\n' ..
+                                          '  "bic": "' .. jsonField(account.bic) .. '",\n' ..
+                                          '  "type": "' .. jsonField(account.type) .. '",\n' ..
+                                          '  "comment": "' .. jsonField(account.comment) .. '",\n' ..
+                                          '  "balance": "' .. jsonField(amount(account.balance)) .. '",\n' ..
+                                          '  "balanceDate": "' .. jsonField(isoDate(account.balanceDate)) .. '",\n' ..
+                                          '  "attributes": [')))
+
+  comma = ""
+  for _,attr in ipairs(account.attributes) do
+    assert(io.write(MM.toEncoding(encoding, comma .. '"' .. jsonField(attr) .. '"')))
+    comma = ", "
+  end
+  assert(io.write(MM.toEncoding(encoding, '],\n  "transactions": [\n')))
   comma = ""
 end
 
 function WriteTransactions (account, transactions)
   for _,transaction in ipairs(transactions) do
 
-    assert(io.write(MM.toEncoding(encoding, comma .. '{\n' ..
-                                            '  "id": "' .. jsonField(transaction.id) .. '",\n' ..
-                                            '  "bookingDate": "' .. jsonField(MM.localizeDate(transaction.bookingDate)) .. '",\n' ..
-                                            '  "valueDate": "' .. jsonField(MM.localizeDate(transaction.valueDate)) .. '",\n' ..
-                                            '  "name": "' .. jsonField(transaction.name) .. '",\n' ..
-                                            '  "accountNumber": "' .. jsonField(transaction.accountNumber) .. '",\n' ..
-                                            '  "bankCode": "' .. jsonField(transaction.bankCode) .. '",\n' ..
-                                            '  "purpose": "' .. jsonField(transaction.purpose) .. '",\n' ..
-                                            '  "transactionCode": "' .. jsonField(transaction.transactionCode) .. '",\n' ..
-                                            '  "textKeyExtension": "' .. jsonField(transaction.textKeyExtension) .. '",\n' ..
-                                            '  "purposeCode": "' .. jsonField(transaction.purposeCode) .. '",\n' ..
-                                            '  "comment": "' .. jsonField(transaction.comment) .. '",\n' ..
-                                            '  "bookingKey": "' .. jsonField(transaction.bookingKey) .. '",\n' ..
-                                            '  "bookingText": "' .. jsonField(transaction.bookingText) .. '",\n' ..
-                                            '  "primanotaNumber": "' .. jsonField(transaction.primanotaNumber) .. '",\n' ..
-                                            '  "batchReference": "' .. jsonField(transaction.batchReference) .. '",\n' ..
-                                            '  "returnReason": "' .. jsonField(transaction.returnReason) .. '",\n' ..
-                                            '  "endToEndReference": "' .. jsonField(transaction.endToEndReference) .. '",\n' ..
-                                            '  "mandateReference": "' .. jsonField(transaction.mandateReference) .. '",\n' ..
-                                            '  "creditorId": "' .. jsonField(transaction.creditorId) .. '",\n' ..
-                                            '  "category": "' .. jsonField(string.gsub(transaction.category, [[\]], " - ")) .. '",\n' ..
-                                            '  "amount": "' .. jsonField(MM.localizeAmount("0.00;-0.00", transaction.amount)) .. '",\n' ..
-                                            '  "currency": "' .. jsonField(transaction.currency) .. '"\n}')))
+    assert(io.write(MM.toEncoding(encoding, comma .. '  {\n' ..
+                                            '    "id": ' .. jsonField(transaction.id) .. ',\n' ..
+                                            '    "bookingDate": "' .. jsonField(isoDate(transaction.bookingDate)) .. '",\n' ..
+                                            '    "valueDate": "' .. jsonField(isoDate(transaction.valueDate)) .. '",\n' ..
+                                            '    "name": "' .. jsonField(transaction.name) .. '",\n' ..
+                                            '    "accountNumber": "' .. jsonField(transaction.accountNumber) .. '",\n' ..
+                                            '    "bankCode": "' .. jsonField(transaction.bankCode) .. '",\n' ..
+                                            '    "purpose": "' .. jsonField(transaction.purpose) .. '",\n' ..
+                                            '    "transactionCode": ' .. transaction.transactionCode .. ',\n' ..
+                                            '    "textKeyExtension": ' .. transaction.textKeyExtension .. ',\n' ..
+                                            '    "purposeCode": "' .. jsonField(transaction.purposeCode) .. '",\n' ..
+                                            '    "comment": "' .. jsonField(transaction.comment) .. '",\n' ..
+                                            '    "bookingKey": "' .. jsonField(transaction.bookingKey) .. '",\n' ..
+                                            '    "bookingText": "' .. jsonField(transaction.bookingText) .. '",\n' ..
+                                            '    "primanotaNumber": "' .. jsonField(transaction.primanotaNumber) .. '",\n' ..
+                                            '    "batchReference": "' .. jsonField(transaction.batchReference) .. '",\n' ..
+                                            '    "returnReason": "' .. jsonField(transaction.returnReason) .. '",\n' ..
+                                            '    "endToEndReference": "' .. jsonField(transaction.endToEndReference) .. '",\n' ..
+                                            '    "mandateReference": "' .. jsonField(transaction.mandateReference) .. '",\n' ..
+                                            '    "creditorId": "' .. jsonField(transaction.creditorId) .. '",\n' ..
+                                            '    "category": "' .. jsonField(string.gsub(transaction.category, [[\]], " - ")) .. '",\n' ..
+                                            '    "booked": ' .. tostring(transaction.booked) .. ',\n' ..
+                                            '    "checkmark": ' .. tostring(transaction.checkmark) .. ',\n' ..
+                                            '    "amount": "' .. jsonField(amount(transaction.amount)) .. '",\n' ..
+                                            '    "currency": "' .. jsonField(transaction.currency) .. '"\n  }')))
 
     comma = ","
   end
@@ -61,5 +94,5 @@ end
 
 
 function WriteTail (account)
-  assert(io.write("\n]"))
+  assert(io.write("\n  ]\n}\n"))
 end
