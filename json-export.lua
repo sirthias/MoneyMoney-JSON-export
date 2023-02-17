71a4393c82e8332fe08e5cffc6ee90e5dd73c6c2
Mathias <mathias@decodified.com>
2023-02-17 17:15:56 +0100

    Add missing fields

--- json-export.lua
+++ json-export.lua
@@ -7,7 +7,7 @@
 
 local encoding = "UTF-8"
 
-Exporter{version       = 1.00,
+Exporter{version       = 1.01,
          format        = "JSON file",
          fileExtension = "json",
          description   = "Export transactions as JSON file"}
@@ -32,18 +32,26 @@ function WriteTransactions (account, transactions)
   for _,transaction in ipairs(transactions) do
 
     assert(io.write(MM.toEncoding(encoding, comma .. '{\n' ..
+                                            '  "id": "' .. jsonField(transaction.id) .. '",\n' ..
                                             '  "bookingDate": "' .. jsonField(MM.localizeDate(transaction.bookingDate)) .. '",\n' ..
                                             '  "valueDate": "' .. jsonField(MM.localizeDate(transaction.valueDate)) .. '",\n' ..
-                                            '  "category": "' .. jsonField(string.gsub(transaction.category, [[\]], " - ")) .. '",\n' ..
                                             '  "name": "' .. jsonField(transaction.name) .. '",\n' ..
+                                            '  "accountNumber": "' .. jsonField(transaction.accountNumber) .. '",\n' ..
+                                            '  "bankCode": "' .. jsonField(transaction.bankCode) .. '",\n' ..
+                                            '  "purpose": "' .. jsonField(transaction.purpose) .. '",\n' ..
+                                            '  "transactionCode": "' .. jsonField(transaction.transactionCode) .. '",\n' ..
+                                            '  "textKeyExtension": "' .. jsonField(transaction.textKeyExtension) .. '",\n' ..
+                                            '  "purposeCode": "' .. jsonField(transaction.purposeCode) .. '",\n' ..
                                             '  "comment": "' .. jsonField(transaction.comment) .. '",\n' ..
+                                            '  "bookingKey": "' .. jsonField(transaction.bookingKey) .. '",\n' ..
                                             '  "bookingText": "' .. jsonField(transaction.bookingText) .. '",\n' ..
+                                            '  "primanotaNumber": "' .. jsonField(transaction.primanotaNumber) .. '",\n' ..
+                                            '  "batchReference": "' .. jsonField(transaction.batchReference) .. '",\n' ..
                                             '  "returnReason": "' .. jsonField(transaction.returnReason) .. '",\n' ..
                                             '  "endToEndReference": "' .. jsonField(transaction.endToEndReference) .. '",\n' ..
                                             '  "mandateReference": "' .. jsonField(transaction.mandateReference) .. '",\n' ..
                                             '  "creditorId": "' .. jsonField(transaction.creditorId) .. '",\n' ..
-                                            '  "accountNumber": "' .. jsonField(transaction.accountNumber) .. '",\n' ..
-                                            '  "bankCode": "' .. jsonField(transaction.bankCode) .. '",\n' ..
+                                            '  "category": "' .. jsonField(string.gsub(transaction.category, [[\]], " - ")) .. '",\n' ..
                                             '  "amount": "' .. jsonField(MM.localizeAmount("0.00;-0.00", transaction.amount)) .. '",\n' ..
                                             '  "currency": "' .. jsonField(transaction.currency) .. '"\n}')))
 
